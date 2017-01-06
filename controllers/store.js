51ab2973f25aa44958716cd9baff3048866a0b9d
sealday <sealday@gmail.com>
2017-01-06 15:53:49 +0800

    添加项目直接查询本项目仓库的库存

--- controllers/store.js
+++ controllers/store.js
@@ -9,35 +9,92 @@ const ObjectId = require('mongoose').Types.ObjectId;
 
 exports.index = (req, res, next) => {
 
-  const projectId = req.query['project'] || '';
+  if (res.locals.current && res.locals.current.type == '基地仓库') {
+    const projectId = req.query['project'] || '';
 
-  if (projectId) {
-    Project.find().then(projects => {
-      res.locals.projects = projects;
-      return StockRecord.aggregate([
-        {
-          $match: {outStock: ObjectId(projectId)},
-        },
-        {
-          $unwind: '$entries'
-        },
-        {
-          $group: {
-            _id: {
-              name: '$entries.name',
-              size: '$entries.size'
-            },
-            sum: {
-              $sum: '$entries.count'
+    if (projectId) {
+      Project.find().then(projects => {
+        res.locals.projects = projects;
+        return StockRecord.aggregate([
+          {
+            $match: {outStock: ObjectId(projectId)},
+          },
+          {
+            $unwind: '$entries'
+          },
+          {
+            $group: {
+              _id: {
+                name: '$entries.name',
+                size: '$entries.size'
+              },
+              sum: {
+                $sum: '$entries.count'
+              }
             }
           }
+        ]);
+      }).then(result => {
+        res.locals.outRecords = result;
+        return StockRecord.aggregate([
+          {
+            $match: {inStock: ObjectId(projectId)},
+          },
+          {
+            $unwind: '$entries'
+          },
+          {
+            $group: {
+              _id: {
+                name: '$entries.name',
+                size: '$entries.size'
+              },
+              sum: {
+                $sum: '$entries.count'
+              }
+            }
+          }
+        ])
+      }).then(result => {
+        res.locals.inRecords = result;
+        return Project.findById(projectId);
+      }).then(project => {
+        res.locals.project = project;
+        res.render('store/index');
+      });
+
+
+    } else {
+      Project.find().then(projects => {
+        res.render('store/index', {
+          projects
+        });
+      });
+    }
+  } else if (res.locals.current && res.locals.current.type == '项目部仓库') {
+    StockRecord.aggregate([
+      {
+        $match: {outStock: ObjectId(res.locals.current._id)},
+      },
+      {
+        $unwind: '$entries'
+      },
+      {
+        $group: {
+          _id: {
+            name: '$entries.name',
+            size: '$entries.size'
+          },
+          sum: {
+            $sum: '$entries.count'
+          }
         }
-      ]);
-    }).then(result => {
+      }
+    ]).then(result => {
       res.locals.outRecords = result;
       return StockRecord.aggregate([
         {
-          $match: {inStock: ObjectId(projectId)},
+          $match: {inStock: ObjectId(res.locals.current._id)},
         },
         {
           $unwind: '$entries'
@@ -56,18 +113,8 @@ exports.index = (req, res, next) => {
       ])
     }).then(result => {
       res.locals.inRecords = result;
-      return Project.findById(projectId);
-    }).then(project => {
-      res.locals.project = project;
+      res.locals.project = res.locals.current;
       res.render('store/index');
-    });
-
-
-  } else {
-    Project.find().then(projects => {
-      res.render('store/index', {
-        projects
-      });
     });
   }
 };
