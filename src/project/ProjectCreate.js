455ce3d1feb88a11841a70ca203cbf4d5d7ba91b
sealday <sealday@gmail.com>
2017-01-19 23:09:35 +0800

    项目编辑录入

--- src/project/ProjectCreate.js
+++ src/project/ProjectCreate.js
@@ -3,6 +3,7 @@
  */
 
 import React, { Component } from 'react';
+import Contact from './Contact'
 
 class ProjectCreate extends Component {
   constructor(props) {
@@ -139,57 +140,6 @@ class ProjectCreate extends Component {
             </div>
           </div>
         </form>
-      </div>
-    );
-  }
-}
-
-class Contact extends Component {
-  static propTypes = {
-    onAdd: React.PropTypes.func,
-    onRemove: React.PropTypes.func,
-    onChange: React.PropTypes.func,
-    id: React.PropTypes.number,
-    name: React.PropTypes.string,
-    phone: React.PropTypes.string,
-  };
-
-  constructor(props) {
-    super(props);
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(e) {
-    this.props.onChange(
-      this.props.id,
-      e.target.name,
-      e.target.value
-    );
-  }
-
-  render() {
-    return (
-      <div className="form-group">
-        <label className="control-label col-sm-2">联系人<span className="important-star">(*)</span></label>
-        <div className="col-sm-3">
-          <input className="form-control" name="name" type="text" value={this.props.name} required onChange={this.handleChange} />
-        </div>
-        <label className="control-label col-sm-2">联系人电话<span className="important-star">(*)</span></label>
-        <div className="col-sm-5">
-          <div className="input-group">
-            <input className="form-control" name="phone" type="text" value={this.props.phone} required onChange={this.handleChange} />
-            <span className="input-group-btn">
-              <a className="btn btn-default" onClick={this.props.onAdd}>
-                <span className="glyphicon glyphicon-plus"/>
-              </a>
-            </span>
-            <span className="input-group-btn">
-              <a className="btn btn-default" onClick={e => this.props.onRemove(this.props.id)}>
-                <span className="glyphicon glyphicon-minus"/>
-              </a>
-            </span>
-          </div>
-        </div>
       </div>
     );
   }
