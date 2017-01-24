4d9e8d1d84a34db54a30a9681afa818645c4d503
sealday <sealday@gmail.com>
2017-01-24 15:42:02 +0800

    删除frontend

--- frontend/src/report/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by seal on 20/01/2017.
- */
-
-export { default as TransferInTable } from './TransferInTable'
-export { default as TransferOutTable } from './TransferOutTable'
-export { default as Store } from './Store'
