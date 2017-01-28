f7e7f4af4e7e528d785a9bdf58e338b00835be43
sealday <sealday@gmail.com>
2017-01-28 20:46:48 +0800

    运输单初步

--- frontend_v2/src/store/TransportOrder.js
+++ frontend_v2/src/store/TransportOrder.js
@@ -3,6 +3,10 @@
  */
 
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { requestRecord } from '../actions'
+import { toFixedWithoutTrailingZero } from '../utils'
+import moment from 'moment'
 
 class TransportOrder extends Component {
   handleEdit = () => {
@@ -13,7 +17,50 @@ class TransportOrder extends Component {
     this.props.router.goBack()
   }
 
+  componentDidMount() {
+    const record = this.props.record
+    if (!record) {
+      this.props.dispatch(requestRecord(this.props.id))
+    }
+  }
+
   render() {
+    const record = this.props.record
+    if (!record) {
+      return <div>请求运输单！</div>
+    }
+
+    if (!record.hasTransport) {
+      return  (
+        <div>
+          <h2>还未填写运输单！</h2>
+          <button className="btn btn-primary hidden-print" onClick={this.handleEdit}>填写运输单</button>
+        </div>
+      )
+    }
+
+    const transport = record.transport
+
+    //'arrival-date'     : moment().startOf('day').add(1, 'day'), // 到达日期
+    //weight             : '', // 重量
+    //price              : '', // 价格
+    //payer              : '', // 付款方
+    //'pay-info'         : '', // 付款信息
+    //payee              : '', // 收款人
+    //bank               : '', // 银行
+    //'delivery-party'   : '', // 发货单位
+    //'delivery-contact' : '', // 发货人
+    //'delivery-phone'   : '', // 发货人电话
+    //'delivery-address' : '', // 发货地址
+    //'receiving-party'  : '', // 收货单位
+    //'receiving-contact': '', // 收货联系
+    //'receiving-phone'  : '', // 收货人电话
+    //'receiving-address': '', // 收货地址
+    //'carrier-party'    : '', // 运输公司
+    //'carrier-name'     : '', // 司机名称
+    //'carrier-phone'    : '', // 司机电话
+    //'carrier-id'       : '', // 司机身份证号码
+    //'carrier-car'      : '', // 车牌号
     return (
       <div>
         <button className="btn btn-default hidden-print" onClick={this.handleBack}>返回</button>
@@ -24,22 +71,22 @@ class TransportOrder extends Component {
           <tr>
             <td>日期</td>
             <td colSpan="2">承运日期</td>
-            <td colSpan="2"></td>
+            <td colSpan="2">{moment(transport['off-date']).format('YYYY-MM-DD')}</td>
             <td>要求到货日期</td>
-            <td></td>
+            <td>{moment(transport['arrival-date']).format('YYYY-MM-DD')}</td>
           </tr>
           <tr>
             <td>货物名称及数量</td>
-            <td colSpan="6"></td>
+            <td colSpan="6"> </td>
           </tr>
           <tr>
             <td>单价</td>
-            <td></td>
+            <td>{transport.price}</td>
             <td>吨/趟</td>
-            <td></td>
+            <td>{transport.weight}</td>
             <td>（元）</td>
             <td>金额</td>
-            <td></td>
+            <td>{toFixedWithoutTrailingZero(transport.price * transport.weight)}</td>
           </tr>
           <tr>
             <td rowSpan="2">付款方式及收款人信息</td>
@@ -50,7 +97,7 @@ class TransportOrder extends Component {
           </tr>
           <tr>
             <td>收款人</td>
-            <td></td>
+            <td>{transport.payer}</td>
             <td colSpan="4"></td>
           </tr>
           <tr>
@@ -109,4 +156,13 @@ class TransportOrder extends Component {
   }
 }
 
-export default TransportOrder;
+const mapStateToProps = (state, props) => {
+  const id = props.params.id
+  const record = state.store.records.get(id)
+  return {
+    record,
+    id
+  }
+}
+
+export default connect(mapStateToProps)(TransportOrder);
--- frontend_v2/src/store/TransportOrderEdit.js
+++ frontend_v2/src/store/TransportOrderEdit.js
@@ -6,6 +6,7 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import moment from 'moment'
 import TransportForm from './TransportForm'
+import { ajax } from '../utils'
 
 class TransportOrderEdit extends Component {
   constructor(props) {
@@ -39,8 +40,26 @@ class TransportOrderEdit extends Component {
   componentDidMount() {
     // 假设本地没有这个缓存，那么属于直接访问这个页面的情况，这时候跳转到订单页面
     if (!this.props.recordIdMap[this.props.params.id]) {
-      this.props.router.push(`/transfer_order/${this.props.params.id}`)
+      this.props.router.push(`/transport/${this.props.params.id}`)
     }
+
+    const record = this.props.recordIdMap[this.props.params.id]
+    const projects = this.props.projects
+    const inStock = projects[record.inStock]
+    const outStock = projects[record.outStock]
+
+    this.setState({
+      'off-date'         : moment(record.outDate).startOf('day'),
+      'arrival-date'     : moment(record.outDate).startOf('day').add(1, 'day'), // 到达日期
+      'delivery-party'   : outStock.company + outStock.name, // 发货单位
+      'delivery-contact' : outStock.contacts[0].name, // 发货人
+      'delivery-phone'   : outStock.contacts[0].phone, // 发货人电话
+      'delivery-address' : outStock.address, // 发货地址
+      'receiving-party'  : inStock.company + inStock.name, // 收货单位
+      'receiving-contact': inStock.contacts[0].name, // 收货联系
+      'receiving-phone'  : inStock.contacts[0].phone, // 收货人电话
+      'receiving-address': inStock.address, // 收货地址
+    })
   }
 
   handleCancel = e => {
@@ -49,25 +68,43 @@ class TransportOrderEdit extends Component {
   }
 
   handleSubmit = data => {
-    alert(JSON.stringify(data))
+    ajax(`/api/transfer/${this.props.params.id}/transport`, {
+      data: JSON.stringify(data),
+      method: 'POST',
+      contentType: 'application/json'
+    }).then(res => {
+      // 更新缓存中的数据
+      this.props.dispatch({ type: 'UPDATE_RECORDS_CACHE', record: res.data.record })
+      this.props.router.push(`/transport/${res.data.record._id}`)
+    }).catch(err => {
+      alert('出错了' + JSON.stringify(err));
+    });
   }
 
   render() {
-    return (
-      <div>
-        <h2>
-          <button className="btn btn-default" onClick={this.handleCancel}>取消编辑</button>
-          <span>运输单编辑</span>
-        </h2>
-        <TransportForm initialValues={this.state} />
-      </div>
-    );
+    if (this.state['delivery-party']) {
+      return (
+        <div>
+          <h2>
+            <button className="btn btn-default" onClick={this.handleCancel}>取消编辑</button>
+            <span>运输单编辑</span>
+          </h2>
+          <TransportForm
+            onSubmit={this.handleSubmit}
+            initialValues={this.state}
+          />
+        </div>
+      )
+    } else {
+      return null
+    }
   }
 }
 
 const mapStateToProps = state => {
   return {
-    recordIdMap: state.recordIdMap,
+    recordIdMap: state.store.records.toObject(),
+    projects: state.system.projects.toObject(),
   }
 }
 
