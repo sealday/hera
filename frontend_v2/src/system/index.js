ed34c4c100b9cc5beeaf799660283e9698da1c2d
sealday <sealday@gmail.com>
2017-01-29 14:30:03 +0800

    转移项目数据修改的位置

--- frontend_v2/src/system/index.js
+++ frontend_v2/src/system/index.js
@@ -6,3 +6,6 @@ export { default as Article } from './Article';
 export { default as Operator } from './Operator'
 export { default as OperatorCreate } from './OperatorCreate'
 export { default as OperatorEdit } from './OperatorEdit'
+export { default as Project } from './Project';
+export { default as ProjectCreate } from './ProjectCreate';
+export { default as ProjectEdit } from './ProjectEdit'
