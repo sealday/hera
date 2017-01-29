8c2bdd4050d233904a14e669bb70520c29cf3d25
xinglianfly <26504546352@qq.com>
2017-01-29 17:59:51 +0800

    '添加进场工人登记卡后台'

---
+++ server/api/worker.js
@@ -0,0 +1,51 @@
+/**
+ * Created by wangjiabao on 2017/1/29.
+ */
+
+const Worker = require('../models/Worker')
+
+exports.list = (req,res,next)=>{
+    Worker.find().then(workers=>{
+        res.json({
+            data:{
+                workers
+            }
+        });
+    }).catch(err=>{
+        next(err);
+    })
+}
+
+
+exports.create = (req,res,next)=>{
+    let worker = new Worker(req.body);
+    worker.save().then(()=>{
+        res.json({
+            message:'保存成功'
+        })
+    }).catch(err=>{
+        next(err);
+    })
+}
+
+exports.update = (req,res,next)=>{
+    const id = req.params.id;
+    Worker.findByIdAndUpdate(id,req.body).then(()=>{
+        res.json({
+            message:'更新成功'
+        })
+    }).catch(err=>{
+        next(err);
+    })
+}
+
+exports.delete =(req,res,next)=>{
+    const id = req.params.id;
+    Worker.findByIdAndRemove(id).then(()=>{
+        res.json({
+            message:'删除成功'
+        })
+    }).catch(err=>{
+        next(err);
+    })
+}
