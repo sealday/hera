29288e60ef6f53a0696d6c13af944682ca712b35
xinglianfly <2650454635@qq.com>
2017-01-29 17:59:51 +0800

    '添加进场工人登记卡后台'

---
+++ server/models/Worker.js
@@ -0,0 +1,19 @@
+/**
+ * Created by wangjiabao on 2017/1/29.
+ */
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const WorkerSchema = new Schema({
+    name:String,//姓名
+    age:Number,//年龄
+    address:String,//家庭住址
+    idcard:String,//身份证号
+    gender:String,//性别
+    phone:String,//电话号码
+    category:String,//工种
+    birthday:Date,//出生日期
+    picture:String//身份证照片
+})
+
