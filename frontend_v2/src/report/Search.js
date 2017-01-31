b56aea940aae1740625e7258b3bd5510d3c45b05
sealday <sealday@gmail.com>
2017-01-31 23:59:12 +0800

    出入库按条件查询

--- frontend_v2/src/report/Search.js
+++ frontend_v2/src/report/Search.js
@@ -7,10 +7,14 @@ import SearchForm from './SearchForm'
 import SearchTable from './SearchTable'
 import { connect } from 'react-redux'
 import { storeSearch } from '../actions'
+import moment from 'moment'
 
 class Search extends React.Component {
   search = condition => {
-    this.props.dispatch(storeSearch(condition))
+    this.props.dispatch(storeSearch({
+      ...condition,
+      endDate: moment(condition.endDate).add(1, 'day')
+    }))
   }
 
   render() {
