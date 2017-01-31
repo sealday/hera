91c3f36c4d58b6d75ad1e0fabceead099cb5a51d
sealday <sealday@gmail.com>
2017-01-31 10:50:51 +0800

    查询全部记录

---
+++ server/database/stockRecordTest4.js
@@ -0,0 +1,44 @@
+/**
+ * Created by seal on 31/12/2016.
+ */
+
+const mongoose = require('mongoose');
+const Record = require('../models/Record').Record;
+const Project = require('../models/Project');
+const Order = require('../models/Order');
+const ObjectId = mongoose.Types.ObjectId;
+
+mongoose.Promise = global.Promise;
+const connection = mongoose.connect('mongodb://localhost/hera');
+
+let start = null;
+connection.then(() => {
+  let outStock = '586df7fe2d256304867ab346';
+
+  start = new Date();
+
+  return Record.aggregate([
+    {
+      $match: {outStock: ObjectId(outStock)},
+    },
+    {
+      $unwind: '$entries'
+    },
+    {
+      $project: {
+        name: '$entries.name',
+        size: '$entries.size',
+        count: '$entries.count',
+      }
+    }
+  ]);
+}).then(results => {
+  let end = new Date() - start;
+  console.log(results);
+  console.log('一共耗时 %d 毫秒', end);
+}).catch(err => {
+  console.log(err);
+}).then(() => {
+  mongoose.disconnect();
+});
+
