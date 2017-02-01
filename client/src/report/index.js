4eb645cc05a00e788f59b34eeec14bad3f5ccfcb
sealday <sealday@gmail.com>
2017-02-01 21:22:22 +0800

    采购、销售明细表

--- client/src/report/index.js
+++ client/src/report/index.js
@@ -4,5 +4,7 @@
 
 export { default as TransferInTable } from './TransferInTable'
 export { default as TransferOutTable } from './TransferOutTable'
+export { default as PurchaseTable } from './PurchaseTable'
+export { default as SellTable } from './SellTable'
 export { default as Search }  from './Search'
 export { default as Store } from './Store'
