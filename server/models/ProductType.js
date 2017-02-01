a4511aae556f5d48292fc02b271e6ce804216093
sealday <sealday@gmail.com>
2017-02-01 20:33:16 +0800

    将pinyin的实现移动到后端去

--- server/models/ProductType.js
+++ server/models/ProductType.js
@@ -2,6 +2,7 @@
  * Created by seal on 26/12/2016.
  */
 const mongoose = require('mongoose');
+const pinyin = require('pinyin')
 
 const Schema = mongoose.Schema;
 const productTypeSchema = new Schema({
@@ -12,9 +13,17 @@ const productTypeSchema = new Schema({
   sizeUnit: String,
   countUnit: String,
   convert: Number, // 折合数量级
-  convertUnit: String // 折合单位
+  convertUnit: String, // 折合单位
+
+  pinyin: String, // 产品名称的拼音
 });
 
+
+productTypeSchema.pre('save', function(next) {
+  this.pinyin = pinyin(this.name,
+    {style: pinyin.STYLE_NORMAL, heteronym: true}).map(array => array.join('')).join('')
+  next()
+})
 
 const ProductType = mongoose.model('ProductType', productTypeSchema);
 
