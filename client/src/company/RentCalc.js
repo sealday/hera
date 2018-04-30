0a313e09f60fc11065a4dcd7807bc243df70c005
sealday <sealday@gmail.com>
2018-04-30 14:15:08 +0800

    update: excel 导出格式

--- client/src/company/RentCalc.js
+++ client/src/company/RentCalc.js
@@ -7,6 +7,7 @@ import { queryRent } from '../actions'
 import * as XLSX from 'xlsx'
 import { saveAs } from 'file-saver'
 import { RENT } from '../actions'
+import { dateFormat } from '../utils'
 
 
 class RentCalc extends React.Component {
@@ -25,15 +26,58 @@ class RentCalc extends React.Component {
         <RentCalcForm
           onSubmit={this.handleSubmit}
           onExcelExport={() => {
-            const wb = XLSX.utils.table_to_book(this.table)
-            for (const sheetName of wb.SheetNames) {
-              const sheet = wb.Sheets[sheetName]
-              for (const i in sheet) {
-                if (sheet[i] && sheet[i].t === 's') {
-                  sheet[i].v = sheet[i].v.replace('￥', '')
+            const wb = XLSX.utils.book_new();
+
+            const json = [[
+              '日期', '出入库', '名称', '规格', '单位', '数量', '单价', '天数', '金额', '运费'
+            ]]
+            for (const item of rent.history) {
+              json.push([
+                '上期结存', null, item.name, '', item.unit,
+                item.count, item.unitPrice || 0, item.days, item.price, 0
+              ])
+            }
+            for (const item of rent.list) {
+              json.push([
+                dateFormat(item.outDate), item.inOut, item.name, item.size, item.unit,
+                item.count, item.unitPrice || 0, item.days, item.price, item.freight || 0
+              ])
+            }
+            const sheet = XLSX.utils.aoa_to_sheet(json)
+            const range = XLSX.utils.decode_range(sheet['!ref'])
+
+            // 数量 5
+            // 单价 6
+            // 金额 8
+            // 运费 9
+            for(let C = range.s.c; C <= range.e.c; ++C) {
+              let format
+              switch (C) {
+                case 5:
+                  format = '#,##0.00_ ;-#,##0.00'
+                  break
+                case 6:
+                  format = '#,##0.0000_ '
+                  break
+                case 8:
+                  format = '#,##0.00_ ;[red]-#,##0.00'
+                  break
+                case 9:
+                  format = '#,##0.00_ ;-#,##0.00'
+                  break
+                default:
+                  continue
+              }
+              for(let R = range.s.r; R <= range.e.r; ++R) {
+                if (R === 0) continue;
+                const cell_address = {c:C, r:R};
+                const cell_ref = XLSX.utils.encode_cell(cell_address);
+                if (sheet[cell_ref]) {
+                  sheet[cell_ref] = XLSX.utils.cell_set_number_format(sheet[cell_ref], format)
                 }
               }
             }
+            XLSX.utils.book_append_sheet(wb, sheet, '租金计算表')
             const out = XLSX.write(wb, { bookType:'xlsx', bookSST:false, type:'binary', compression: true })
             const s2ab = (s) => {
               const buf = new ArrayBuffer(s.length)
