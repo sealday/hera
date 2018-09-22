05cdab3b4bd9a79bf09a495c3403f7b9267cff46
sealday <sealday@gmail.com>
2018-09-22 13:21:42 +0800

    update: 更新合同详情界面

--- client/src/company/ContractContent.js
+++ client/src/company/ContractContent.js
@@ -3,15 +3,27 @@ import React from 'react';
 import { connect } from 'react-redux'
 import Card from '@material-ui/core/Card'
 import CardContent from '@material-ui/core/CardContent'
+import CardHeader from '@material-ui/core/CardHeader'
 import { currencyFormat, dateFormat } from '../utils'
 import Typography from '@material-ui/core/Typography'
+import Button from '@material-ui/core/Button'
 import short_id from 'shortid'
 import { withStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import Paper from '@material-ui/core/Paper'
 
 const styles = {
   title: {
     fontSize: 42,
   },
+  content: {
+    marginTop: '2em',
+    padding: '1em',
+  }
 };
 
 class ContractContent extends React.Component {
@@ -22,51 +34,51 @@ class ContractContent extends React.Component {
   }
 
   render() {
-    let { projects, params, classes } = this.props
+    let { projects, params, classes, router } = this.props
 
     const project = projects.get(params.id)
-    return (
-      <div>
-        <h2 className="page-header">合同详情</h2>
+    return [
         <Card>
+          <CardHeader
+            action={
+                <Button onClick={() => router.goBack()}>返回</Button>
+            }
+            title={`${project.company} ${project.name}`}
+            subheader={`内部编号：${project._id} 外部编号：${short_id.generate()}`}
+          />
           <CardContent>
-            <Typography
-              className={classes.title}
-              variant="headline"
-              component="h2"
-            >{ project.company } { project.name }</Typography>
             <Typography variant="subheading" color="textSecondary">
-              <p>编号：{ project._id }  <br/>外部：{ short_id.generate() }</p>
             </Typography>
             <p>费用：<span style={{ fontSize: 'xx-large' }}>{ currencyFormat(761946.14) }</span> </p>
             <p>收款：<span style={{ fontSize: 'xx-large' }}>{ currencyFormat(150000.00) }</span> </p>
             <p>税收：<span style={{ fontSize: 'xx-large' }}>{ currencyFormat(232.23) }</span> </p>
           </CardContent>
-        </Card>
-        <table className="table table-bordered" style={{ marginTop: '2em' }}>
-          <thead>
-          <tr>
-            <th>名称</th>
-            <th>日期区间</th>
-            <th>创建时间</th>
-            <th>更新时间</th>
-            <th>操作员</th>
-          </tr>
-          </thead>
-          <tbody>
-          {project.items.map(item => (
-            <tr key={item._id} id={item._id}>
-              <td>{item.name}</td>
-              <td>{dateFormat(item.startDate)} ~ {dateFormat(item.endDate)}</td>
-              <td>{dateFormat(item.createdAt)}</td>
-              <td>{dateFormat(item.updatedAt)}</td>
-              <td>{item.username}</td>
-            </tr>
-          ))}
-          </tbody>
-        </table>
-      </div>
-    );
+        </Card>,
+        <Paper className={classes.content}>
+          <Table>
+            <TableHead>
+              <TableCell>名称</TableCell>
+              <TableCell>日期区间</TableCell>
+              <TableCell>创建时间</TableCell>
+              <TableCell>更新时间</TableCell>
+              <TableCell>操作员</TableCell>
+              <TableCell/>
+            </TableHead>
+            <TableBody>
+              {project.items.map(item => (
+                <TableRow key={item._id} id={item._id}>
+                  <TableCell>{item.name}</TableCell>
+                  <TableCell>{dateFormat(item.startDate)} ~ {dateFormat(item.endDate)}</TableCell>
+                  <TableCell>{dateFormat(item.createdAt)}</TableCell>
+                  <TableCell>{dateFormat(item.updatedAt)}</TableCell>
+                  <TableCell>{item.username}</TableCell>
+                  <TableCell><Button>查看</Button></TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+    ]
   }
 }
 
