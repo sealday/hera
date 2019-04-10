ba03e0dadefd12cb23785c4b818f48b46a1de880
sealday <sealday@gmail.com>
2019-04-11 06:09:26 +0800

    update: 用户信息界面

--- client/src/store/TransportForm.js
+++ client/src/store/TransportForm.js
@@ -25,61 +25,61 @@ class TransportForm extends React.Component {
         <CardContent>
           <form className="form-horizontal" onSubmit={handleSubmit}>
             <div className="form-group">
-              <label className="col-sm-2 control-label">承租日期</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">承租日期</label>
+              <div className="col-md-4">
                 <Field name="off-date" component={DatePicker}/>
               </div>
-              <label className="col-sm-2 control-label">到货日期</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">到货日期</label>
+              <div className="col-md-4">
                 <Field name="arrival-date" component={DatePicker}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">吨/趟</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">吨/趟</label>
+              <div className="col-md-4">
                 <Field name="weight" component={Input}/>
               </div>
-              <label className="col-sm-2 control-label">单价</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">单价</label>
+              <div className="col-md-4">
                 <Field name="price" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">附加价格</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">附加价格</label>
+              <div className="col-md-4">
                 <Field name="extraPrice" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">付款方</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">付款方</label>
+              <div className="col-md-4">
                 <Field name="payer" component={Select}>
                   <option>{this.props.optionA}</option>
                   <option>{this.props.optionB}</option>
                 </Field>
               </div>
-              <label className="col-sm-2 control-label">付款约定</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">付款约定</label>
+              <div className="col-md-4">
                 <Field name="pay-info" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">付款日期</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">付款日期</label>
+              <div className="col-md-4">
                 <Field name="payDate" component={DatePicker}/>
               </div>
-              <label className="col-sm-2 control-label">收款人</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">收款人</label>
+              <div className="col-md-4">
                 <Field name="payee" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">收款人开户行</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">收款人开户行</label>
+              <div className="col-md-4">
                 <Field name="bank" component={Input}/>
               </div>
-              <label className="col-sm-2 control-label">收款人账号</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">收款人账号</label>
+              <div className="col-md-4">
                 <Field
                   name="account"
                   component={MaskedInput}
@@ -95,50 +95,50 @@ class TransportForm extends React.Component {
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">发货方联系人</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">发货方联系人</label>
+              <div className="col-md-4">
                 <Field name="delivery-contact" component={Input}/>
               </div>
-              <label className="col-sm-2 control-label">发货方联系电话</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">发货方联系电话</label>
+              <div className="col-md-4">
                 <Field name="delivery-phone" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">收货方联系人</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">收货方联系人</label>
+              <div className="col-md-4">
                 <Field name="receiving-contact" component={Input}/>
               </div>
-              <label className="col-sm-2 control-label">收货方联系电话</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">收货方联系电话</label>
+              <div className="col-md-4">
                 <Field name="receiving-phone" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">承运方单位</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">承运方单位</label>
+              <div className="col-md-4">
                 <Field name="carrier-party" component={Input}/>
               </div>
-              <label className="col-sm-2 control-label">承运方司机</label>
-              <div className="col-sm-4">
+              <label className="col-md-2 control-label">承运方司机</label>
+              <div className="col-md-4">
                 <Field name="carrier-name" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">承运方联系号码</label>
-              <div className="col-sm-10">
+              <label className="col-md-2 control-label">承运方联系号码</label>
+              <div className="col-md-10">
                 <Field name="carrier-phone" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">司机身份证号</label>
-              <div className="col-sm-10">
+              <label className="col-md-2 control-label">司机身份证号</label>
+              <div className="col-md-10">
                 <Field name="carrier-id" component={Input}/>
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">收款人信息</label>
-              <div className="col-sm-10">
+              <label className="col-md-2 control-label">收款人信息</label>
+              <div className="col-md-10">
                 <Field name="a" component={Select} onChange={e => {
                   const [bank, name, account] = e.target.value.split(',')
                   change('bank', bank)
@@ -154,8 +154,8 @@ class TransportForm extends React.Component {
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">发货方联系人</label>
-              <div className="col-sm-10">
+              <label className="col-md-2 control-label">发货方联系人</label>
+              <div className="col-md-10">
                 <Field name="b" component={Select} onChange={e => {
                   const [name, phone] = e.target.value.split(',')
                   change('delivery-contact', name)
@@ -168,8 +168,8 @@ class TransportForm extends React.Component {
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">收货方联系人</label>
-              <div className="col-sm-10">
+              <label className="col-md-2 control-label">收货方联系人</label>
+              <div className="col-md-10">
                 <Field name="c" component={Select} onChange={e => {
                   const [name, phone] = e.target.value.split(',')
                   change('receiving-contact', name)
@@ -182,8 +182,8 @@ class TransportForm extends React.Component {
               </div>
             </div>
             <div className="form-group">
-              <label className="col-sm-2 control-label">承运方</label>
-              <div className="col-sm-10">
+              <label className="col-md-2 control-label">承运方</label>
+              <div className="col-md-10">
                 <Field name="d" component={Select} onChange={e => {
                   const [projectId, idx] = e.target.value.split(',')
                   change('carrier-party', projectMap[projectId].company + projectMap[projectId].name)
