bc1ec048abd3bf536746d38c0326a6ed9bb2fe79
sealday <sealday@gmail.com>
2019-07-27 17:30:17 +0800

    update: 线上紧急修复

--- client/src/index.js
+++ client/src/index.js
@@ -128,8 +128,10 @@ ajax('/api/load').then(res => {
 
   try {
     const store_ = JSON.parse(localStorage.getItem(`store-${ config.db }`))
-    console.log(`store-${ config.db }`)
-    store.dispatch(selectStore(store_))
+    if ('_id' in store_) {
+      store.dispatch(selectStore(store_))
+      console.log(`store-${ config.db }`)
+    }
   } catch (e) {
     console.warn(e);
   }
