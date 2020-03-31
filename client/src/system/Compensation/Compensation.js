ab381fcb9a3b10ffb597fa63076fce7b91d5edce
baijingfeng <2596239285@qq.com>
2020-04-01 01:04:29 +0800

    stage: 编写维修方案功能模块

--- client/src/system/Compensation/Compensation.js
+++ client/src/system/Compensation/Compensation.js
@@ -1,94 +1,135 @@
 import React from 'react'
 import moment from 'moment'
-import { Link } from 'react-router'
-import { connect } from 'react-redux'
-import {
-  Button,
-  Card,
-  CardHeader,
-  CardContent,
-} from '@material-ui/core'
-import {
-  Popconfirm,
-} from 'antd'
+import {Link} from 'react-router'
+import {connect} from 'react-redux'
+import {Button, Card, CardHeader, CardContent} from '@material-ui/core'
+import {Table, Popconfirm} from 'antd'
 import 'antd/lib/popconfirm/style/css'
 
-import { ajax } from '../../utils'
-import { newErrorNotify, newInfoNotify, newSuccessNotify, queryCompensationPlan, COMPENSATION_PLAN } from '../../actions'
+import {ajax} from '../../utils'
+import {newErrorNotify, newInfoNotify, newSuccessNotify, queryCompensationPlan, COMPENSATION_PLAN} from '../../actions'
 
 class Compensation extends React.Component {
-  componentDidMount() {
-    this.load()
-  }
+	componentDidMount() {
+		this.load()
+	}
 
-  load = () => {
-    this.props.dispatch(queryCompensationPlan())
-  }
+	load = () => {
+		this.props.dispatch(queryCompensationPlan())
+	}
 
-  handleDelete = (id) => {
-    this.props.dispatch(newInfoNotify('提示', '正在删除', 1000))
-    ajax(`/api/compensation/${ id }/delete`, {
-      method: 'POST',
-      contentType: 'application/json'
-    }).then((res) => {
-      this.props.dispatch(newSuccessNotify('提示', '删除成功', 1000))
-      this.load()
-    }).catch((err) => {
-      this.props.dispatch(newErrorNotify('警告', '删除失败', 1000))
-    })
-  }
+	handleDelete = id => {
+		this.props.dispatch(newInfoNotify('提示', '正在删除', 1000))
+		ajax(`/api/compensation/${id}/delete`, {
+			method: 'POST',
+			contentType: 'application/json'
+		})
+			.then(res => {
+				this.props.dispatch(newSuccessNotify('提示', '删除成功', 1000))
+				this.load()
+			})
+			.catch(err => {
+				this.props.dispatch(newErrorNotify('警告', '删除失败', 1000))
+			})
+	}
 
-  render() {
-    return (
-      <Card>
-        <CardHeader
-          title="维修方案"
-          action={<>
-            <Button color="primary" component={Link} to="/compensation/create">新增</Button>
-          </>}
-        />
-        <CardContent>
-          <table className="table table-bordered" style={{ width: '100%', tableLayout: 'fixed' }}>
-            <thead>
-            <tr>
-              <th>名称</th>
-              <th>日期</th>
-              <th>备注</th>
-              <th/>
-            </tr>
-            </thead>
-            <tbody>
-            {(this.props.plans || []).map((plan) => (
-              <tr key={plan._id}>
-                <td>{plan.name}</td>
-                <td>{moment(plan.date).format('YYYY-MM-DD')}</td>
-                <td>{plan.comments}</td>
-                <td>
-                  <Link component="button" to={`/compensation/${plan._id}`}>编辑</Link>
-                  <Popconfirm
-                    title={`确认删除“${plan.name}”方案吗？`}
-                    onConfirm={() => this.handleDelete(plan._id)}
-                    okText="确认"
-                    cancelText="取消"
-                  >
-                    <Link>删除</Link>
-                  </Popconfirm>
-                  <Link to={`/compensation/create/${plan._id}`}>克隆</Link>
-                </td>
-              </tr>
-            ))}
-            </tbody>
-          </table>
-        </CardContent>
-      </Card>
-    )
-  }
+	columns = [
+		{
+			title: '名称',
+			dataIndex: 'name',
+			key: 'name'
+		},
+		{
+			title: '日期',
+			dataIndex: 'date',
+			key: 'date'
+		},
+		{
+			title: '备注',
+			dataIndex: 'comments',
+			key: 'comments'
+		},
+		{
+			title: '操作',
+			key: 'action',
+			render: (text, record) => (
+				<span>
+					<Link to={`/compensation/${record._id}`}>
+						编辑
+					</Link>
+					<a style={{marginRight: 16}}>Invite {record.name}</a>
+					<a>Delete</a>
+				</span>
+			)
+		}
+	]
+
+	data = [
+		{
+			name: '测试维修方案',
+			date: '20200311',
+			comments: '这里是备注信息'
+		},
+		{
+			name: '测试维修方案',
+			date: '20200311',
+			comments: '这里是备注信息'
+		}
+	]
+
+	render() {
+		return (
+			<Card>
+				<CardHeader
+					title="维修方案"
+					action={
+						<>
+							<Button color="primary" component={Link} to="/compensation/create">
+								新增
+							</Button>
+						</>
+					}
+				/>
+				<CardContent>
+					<Table columns={this.columns} dataSource={this.data || this.props.plans} />
+					<table className="table table-bordered" style={{width: '100%', tableLayout: 'fixed'}}>
+						<thead>
+							<tr>
+								<th>名称</th>
+								<th>日期</th>
+								<th>备注</th>
+								<th />
+							</tr>
+						</thead>
+						<tbody>
+							{(this.props.plans || []).map(plan => (
+								<tr key={plan._id}>
+									<td>{plan.name}</td>
+									<td>{moment(plan.date).format('YYYY-MM-DD')}</td>
+									<td>{plan.comments}</td>
+									<td>
+										<Link component="button" to={`/compensation/${plan._id}`}>
+											编辑
+										</Link>
+										<Popconfirm title={`确认删除“${plan.name}”方案吗？`} onConfirm={() => this.handleDelete(plan._id)} okText="确认" cancelText="取消">
+											<Link>删除</Link>
+										</Popconfirm>
+										<Link to={`/compensation/create/${plan._id}`}>克隆</Link>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+				</CardContent>
+			</Card>
+		)
+	}
 }
 
-const mapStateToProps = (state) => {
-  const plans = state.results.get(COMPENSATION_PLAN, [])
-  return {
-    plans: plans,
-  }
+const mapStateToProps = state => {
+	const plans = state.results.get(COMPENSATION_PLAN, [])
+	return {
+		plans: plans
+	}
 }
 export default connect(mapStateToProps)(Compensation)
