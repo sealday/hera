185ec366e4f9ffed52fc84c913f8a52b8c649136
sealday <sealday@gmail.com>
2022-09-11 04:28:19 +0800

    update: antd

--- client/src/components/DateRangeModifier.js
+++ client/src/components/DateRangeModifier.js
@@ -1,41 +1,45 @@
 import React from 'react'
 import moment from 'moment'
-import {
-  Button,
-} from '@material-ui/core'
+import { Button, ConfigProvider } from 'antd'
 
 const DateRangeModifier = ({ change, key_start, key_end, current }) => (
-  <>
+  <ConfigProvider componentSize='small'>
     <Button
+      type='text'
       onClick={() => {
         change(key_start, moment().startOf('year'))
         change(key_end, moment().endOf('year').startOf('day'))
       }}
     >今年</Button>
     <Button
+      type='text'
       onClick={() => {
         change(key_start, moment(current).add(-1, 'year').startOf('year'))
         change(key_end, moment(current).add(-1, 'year').endOf('year').startOf('day'))
       }}
     >上一年</Button>
     <Button
+      type='text'
       onClick={() => {
         change(key_start, moment(current).add(1, 'year').startOf('year'))
         change(key_end, moment(current).add(1, 'year').endOf('year').startOf('day'))
       }}
     >下一年</Button>
     <Button
+      type='text'
       onClick={() => {
         change(key_start, moment().startOf('day').add(-1, 'month'))
         change(key_end, moment().startOf('day'))
       }}
     >最近一个月</Button>
-    <Button onClick={() => {
-      change(key_start, moment().startOf('day').add(-2, 'month'))
-      change(key_end, moment().startOf('day'))
-    }}
+    <Button
+      type='text'
+      onClick={() => {
+        change(key_start, moment().startOf('day').add(-2, 'month'))
+        change(key_end, moment().startOf('day'))
+      }}
     >两个月</Button>
-  </>
+  </ConfigProvider>
 )
 
 export default DateRangeModifier
