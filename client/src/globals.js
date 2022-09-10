cc96f1cced6cb3d5a423b64d8d011d8de5ab7e3b
sealday <sealday@gmail.com>
2022-09-10 14:00:44 +0800

    update: 写好入口，具体后面实现

--- client/src/globals.js
+++ client/src/globals.js
@@ -1,5 +1,5 @@
 import { reducer as formReducer } from 'redux-form'
-import { configureStore, isRejectedWithValue } from '@reduxjs/toolkit'
+import { configureStore, isFulfilled, isPending, isRejectedWithValue } from '@reduxjs/toolkit'
 import * as reducers from './reducers'
 import coreSlice from './features/coreSlice'
 import { createBrowserHistory } from 'history'
@@ -11,32 +11,40 @@ export const BASENAME = '/system'
 export const history = createBrowserHistory()
 
 export const RECORD_TYPE_MAP = {
-    'purchase': '购销',
-    'rent': '调拨',
-    'check': '盘点',
-    'transfer': '暂存',
+  'purchase': '购销',
+  'rent': '调拨',
+  'check': '盘点',
+  'transfer': '暂存',
 }
 
 const rtkQueryErrorLogger = (api) => (next) => (action) => {
-    // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
-    if (isRejectedWithValue(action)) {
-      console.warn('We got a rejected action!')
-      console.warn(action)
-    }
-  
-    return next(action)
+  // RTK Query uses `createAsyncThunk` from redux-toolkit under the hood, so we're able to utilize these matchers!
+  if (isRejectedWithValue(action)) {
+    console.warn('We got a rejected action!')
+    console.warn(action)
   }
 
+  if (isPending(action)) {
+    // TODO 这里可以记录请求记录
+  }
+  
+  if (isFulfilled(action)) {
+    // TODO 这里可以记录成功
+  }
+
+  return next(action)
+}
+
 export const store = configureStore({
-    reducer: {
-        ...reducers,
-        core: coreSlice,
-        [heraApi.reducerPath]: heraApi.reducer,
-        form: formReducer,
-    },
-    middleware: getDefaultMiddleware => {
-        return getDefaultMiddleware().concat(heraApi.middleware).concat(rtkQueryErrorLogger)
-    },
+  reducer: {
+    ...reducers,
+    core: coreSlice,
+    [heraApi.reducerPath]: heraApi.reducer,
+    form: formReducer,
+  },
+  middleware: getDefaultMiddleware => {
+    return getDefaultMiddleware().concat(heraApi.middleware).concat(rtkQueryErrorLogger)
+  },
 })
 
 setupListeners(store.dispatch)
