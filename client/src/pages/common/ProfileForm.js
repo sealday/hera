312aeea979e950c5733fc8e3aabee86c6a4e6d32
sealday <sealday@gmail.com>
2022-09-16 01:54:13 +0800

    update: profile 界面去 material

--- client/src/pages/common/ProfileForm.js
+++ client/src/pages/common/ProfileForm.js
@@ -1,45 +1,38 @@
 import React from 'react'
 import { Field, reduxForm } from 'redux-form'
-import {
-  Button,
-  Card,
-  CardContent,
-  CardHeader,
-} from '@material-ui/core'
+import { Button, Card } from 'antd'
 
 import { Input } from '../../components'
 
 const ProfileForm = props => (
   <Card>
-    <CardContent>
-      <form onSubmit={props.handleSubmit}>
-        <div className="form-group">
-          <label className="col-md-2 control-label">用户名</label>
-          <div className="col-md-10">
-            <Field name="username" component={Input} validate={value => value ? undefined : '不能为空'} />
-          </div>
+    <form onSubmit={props.handleSubmit}>
+      <div className="form-group">
+        <label className="col-md-2 control-label">用户名</label>
+        <div className="col-md-10">
+          <Field name="username" component={Input} validate={value => value ? undefined : '不能为空'} />
         </div>
-        <div className="form-group">
-          <label className="col-md-2 control-label">密码</label>
-          <div className="col-md-10">
-            <Field name="password" component={Input} type="password" autoComplete={false} />
-          </div>
+      </div>
+      <div className="form-group">
+        <label className="col-md-2 control-label">密码</label>
+        <div className="col-md-10">
+          <Field name="password" component={Input} type="password" autoComplete={false} />
         </div>
-        <div className="form-group">
-          <label className="col-md-2 control-label">姓名</label>
-          <div className="col-md-10">
-            <Field name="profile.name" component={Input} />
-          </div>
+      </div>
+      <div className="form-group">
+        <label className="col-md-2 control-label">姓名</label>
+        <div className="col-md-10">
+          <Field name="profile.name" component={Input} />
         </div>
-        <div className="form-group">
-          <label className="col-md-2 control-label">备注</label>
-          <div className="col-md-10">
-            <Field name="comments" component={Input} />
-          </div>
+      </div>
+      <div className="form-group">
+        <label className="col-md-2 control-label">备注</label>
+        <div className="col-md-10">
+          <Field name="comments" component={Input} />
         </div>
-        <Button color="primary" fullWidth type="submit" variant="outlined">保存</Button>
-      </form>
-    </CardContent>
+      </div>
+      <Button type="primary" block htmlType="submit">保存</Button>
+    </form>
   </Card>
 )
 
