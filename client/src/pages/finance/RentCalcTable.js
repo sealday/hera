c6233983a14effc434be41d4efbb3abeb9ab1312
sealday <sealday@gmail.com>
2022-09-16 09:53:05 +0800

    update: 租金计算

--- client/src/pages/finance/RentCalcTable.js
+++ client/src/pages/finance/RentCalcTable.js
@@ -1,3 +1,4 @@
+import { Card, Table } from 'antd'
 import React from 'react'
 import { connect } from 'react-redux'
 import shortId from 'shortid'
@@ -8,63 +9,51 @@ class SimpleSearchTable extends React.Component {
 
   render() {
     const { rent, onLoad } = this.props
+    const columns = [
+      { key: 'outDate', dataIndex: 'outDate', title: '日期' },
+      { key: 'inOut', dataIndex: 'inOut', title: '出入库' },
+      { key: 'name', dataIndex: 'name', title: '名称' },
+      { key: 'size', dataIndex: 'size', title: '规格' },
+      { key: 'unit', dataIndex: 'unit', title: '单位' },
+      { key: 'count', dataIndex: 'count', title: '数量' },
+      { key: 'unitPrice', dataIndex: 'unitPrice', title: '金额' },
+      { key: 'days', dataIndex: 'days', title: '天数' },
+      { key: 'price', dataIndex: 'price', title: '金额' },
+      { key: 'freight', dataIndex: 'freight', title: '运费' },
+    ]
+    const dataSource = rent.history.map((item) => ({
+      key: shortId.generate(),
+      outDate: '上期结存',
+      inOut: '',
+      name: item.name,
+      size: '',
+      unit: item.unit,
+      count: numberFormat(item.count),
+      unitPrice: currencyFormat(item.unitPrice, 4),
+      days: item.days,
+      price: currencyFormat(item.price),
+    })).concat(rent.list.map((item) => ({
+      ...item,
+      key: shortId.generate(),
+      outDate: dateFormat(item.outDate),
+      count: numberFormat(item.count),
+      unitPrice: currencyFormat(item.unitPrice, 4),
+      price: currencyFormat(item.price),
+      freight: currencyFormat(item.freight),
+    })))
+
 
     return (
-      [
-        <table ref={onLoad} padding="dense" key={0}>
-          <thead>
-            <tr>
-              <td>日期</td>
-              <td>出入库</td>
-              <td>名称</td>
-              <td>规格</td>
-              <td>单位</td>
-              <td>数量</td>
-              <td>单价</td>
-              <td>天数</td>
-              <td>金额</td>
-              <td>运费</td>
-            </tr>
-          </thead>
-          <tbody>
-            {rent.history.map((item) => (
-              <tr key={shortId.generate()}>
-                <td>上期结存</td>
-                <td />
-                <td>{item.name}</td>
-                <td />
-                <td>{item.unit}</td>
-                <td className="text-right">{numberFormat(item.count)}</td>
-                <td className="text-right">{currencyFormat(item.unitPrice, 4)}</td>
-                <td>{item.days}</td>
-                <td className="text-right">{currencyFormat(item.price)}</td>
-                <td />
-              </tr>
-            ))}
-            {rent.list.map((item) => (
-              <tr key={shortId.generate()}>
-                <td>{dateFormat(item.outDate)}</td>
-                <td>{item.inOut}</td>
-                <td>{item.name}</td>
-                <td>{item.size}</td>
-                <td>{item.unit}</td>
-                <td className="text-right">{numberFormat(item.count)}</td>
-                <td className="text-right">{currencyFormat(item.unitPrice, 4)}</td>
-                <td>{item.days}</td>
-                <td className="text-right">{currencyFormat(item.price)}</td>
-                <td className="text-right">{currencyFormat(item.freight)}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>,
-        (rent.list.length > 0 || rent.history.length > 0) && <ul key={1}>
+      <Card bordered={false} >
+        <Table columns={columns} dataSource={dataSource} pagination={null} />
+        {(rent.list.length > 0 || rent.history.length > 0) && <ul key={1}>
           {rent.nameGroup.map(item => (
             <li key={shortId.generate()}>{item.name}： {numberFormat(item.count)} {item.unit}</li>
           ))}
           <li key="price">金额：{numberFormat(rent.group.price)} 元</li>
           <li key="freight">运费：{numberFormat(rent.group.freight)} 元</li>
-        </ul>,
-      ]
+        </ul>}
+      </Card>
     )
   }
 }
