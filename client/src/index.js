abc9ab31352ead5554f1da8917c6da907358afab
sealday <sealday@gmail.com>
2022-09-18 13:35:44 +0800

    fix: 修复兼容性问题 try3

--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,5 @@
+import 'react-app-polyfill/ie11';
+import 'react-app-polyfill/stable';
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
