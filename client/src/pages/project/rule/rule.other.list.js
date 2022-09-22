6977e3968077ed0e8acc2d6830f5ef0e70d1969a
sealday <sealday@gmail.com>
2022-09-22 21:38:10 +0800

    update: 限制合同选择的计算类型

--- client/src/pages/project/rule/rule.other.list.js
+++ client/src/pages/project/rule/rule.other.list.js
@@ -1,7 +1,6 @@
 import { MinusCircleOutlined, PlusCircleOutlined } from "@ant-design/icons"
-import { Button, Card, Col, DatePicker, Form, Input, Row, Select } from "antd"
+import { Button, Card, Col, Form, Input, Row, Select } from "antd"
 import { RefCascader } from "../../../components"
-import { RULE_CATEGORIES } from "../../../constants"
 import { buildProductTree } from "../../../utils"
 
 const rules = [{ required: true }]
@@ -14,102 +13,119 @@ const styles = {
     fontWeight: '500',
   },
 }
-export default ({ form }) => (
-  <Card style={styles.keepSpace} title='规则明细'>
-    <Row gutter={8}>
-      <Col style={styles.titleContainer} flex='70px'>层级</Col>
-      <Col style={styles.titleContainer} flex='300px'>关联产品</Col>
-      <Col style={styles.titleContainer} flex='200px'>计费项目</Col>
-      <Col style={styles.titleContainer} flex='100px'>单价</Col>
-      <Col style={styles.titleContainer} flex='110px'>计算类型</Col>
-      <Col style={styles.titleContainer} flex='auto'>备注</Col>
-      <Col style={styles.titleContainer} flex='20px'></Col>
-    </Row>
-    <Form.List name='items'>
-      {(fields, { add, remove }) => {
-        return (
-          <>{fields.map(field => (
-            <Row gutter={8} key={field.key}>
-              <Col flex='70px'>
-                <Form.Item {...field} name={[field.name, 'level']}>
-                  <Select options={[
-                    { label: '产品', value: '产品' },
-                    { label: '规格', value: '规格' },
-                    { label: '按单', value: '按单' },
-                  ]} />
-                </Form.Item>
-              </Col>
-              <Col flex='300px'>
-                <Form.Item noStyle dependencies={[['items', field.name, 'level']]}>
-                  {() => {
-                    const level = form.getFieldValue(['items', field.name, 'level'])
-                    const product = form.getFieldValue(['items', field.name, 'associate'])
-                    if (level === '产品') {
-                      if (product && product.length === 3) {
-                        form.setFieldValue(['items', field.name, 'associate'], product.slice(0, 2))
+export default () => {
+  const form = Form.useFormInstance()
+  return (
+    <Card style={styles.keepSpace} title='规则明细'>
+      <Row gutter={8}>
+        <Col style={styles.titleContainer} flex='70px'>层级</Col>
+        <Col style={styles.titleContainer} flex='300px'>关联产品</Col>
+        <Col style={styles.titleContainer} flex='200px'>计费项目</Col>
+        <Col style={styles.titleContainer} flex='100px'>单价</Col>
+        <Col style={styles.titleContainer} flex='110px'>计算类型</Col>
+        <Col style={styles.titleContainer} flex='auto'>备注</Col>
+        <Col style={styles.titleContainer} flex='20px'></Col>
+      </Row>
+      <Form.List name='items'>
+        {(fields, { add, remove }) => {
+          return (
+            <>{fields.map(field => (
+              <Row gutter={8} key={field.key}>
+                <Col flex='70px'>
+                  <Form.Item {...field} name={[field.name, 'level']}>
+                    <Select options={[
+                      { label: '产品', value: '产品' },
+                      { label: '规格', value: '规格' },
+                      { label: '按单', value: '按单' },
+                    ]} />
+                  </Form.Item>
+                </Col>
+                <Col flex='300px'>
+                  <Form.Item noStyle dependencies={[['items', field.name, 'level']]}>
+                    {() => {
+                      const level = form.getFieldValue(['items', field.name, 'level'])
+                      const product = form.getFieldValue(['items', field.name, 'associate'])
+                      if (level === '产品') {
+                        if (product && product.length === 3) {
+                          form.setFieldValue(['items', field.name, 'associate'], product.slice(0, 2))
+                        }
+                        return <RefCascader
+                          item={{ required: true, name: [field.name, 'associate'], option: { ref: 'product' } }}
+                          customBuild={data => buildProductTree(data, false).children} />
+                      } else if (level === '规格') {
+                        return <RefCascader
+                          item={{ required: true, name: [field.name, 'associate'], option: { ref: 'product' } }}
+                          customBuild={data => buildProductTree(data).children} />
+                      } else if (level === '按单') {
+                        return (
+                          <Form.Item name={[field.name, 'condition']}>
+                            <Select options={[
+                              { label: '出入库', value: '出入库' },
+                              { label: '出库', value: '出库' },
+                              { label: '入库', value: '入库' },
+                              { label: '合同运费', value: '合同运费' },
+                            ]} />
+                          </Form.Item>
+                        )
+
                       }
-                      return <RefCascader
-                        item={{ required: true, name: [field.name, 'associate'], option: { ref: 'product' } }}
-                        customBuild={data => buildProductTree(data, false).children} />
-                    } else if (level === '规格') {
-                      return <RefCascader
-                        item={{ required: true, name: [field.name, 'associate'], option: { ref: 'product' } }}
-                        customBuild={data => buildProductTree(data).children} />
-                    } else if (level === '按单') {
+                    }}
+                  </Form.Item>
+                </Col>
+                <Col flex='200px'>
+                  <RefCascader
+                    item={{ required: true, name: [field.name, 'other'], option: { ref: 'other', label: 'name', value: 'id' } }} />
+                </Col>
+                <Col flex='100px'>
+                  <Form.Item {...field} name={[field.name, 'unitPrice']}>
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col flex='110px'>
+                  <Form.Item noStyle dependencies={[['items', field.name, 'level']]}>
+                    {() => {
+                      const options = []
+                      const level = form.getFieldValue(['items', field.name, 'level'])
+                      if (level !== '按单') {
+                        options.push(
+                          { label: '数量', value: '数量' },
+                          { label: '换算数量', value: '换算数量' },
+                          { label: '重量', value: '重量' },
+                        )
+                      } else {
+                        options.push(
+                          { label: '重量', value: '重量' },
+                          { label: '实际重量', value: '实际重量' },
+                        )
+                      }
                       return (
-                        <Form.Item name={[field.name, 'condition']}>
-                          <Select options={[
-                            { label: '出入库', value: '出入库' },
-                            { label: '出库', value: '出库' },
-                            { label: '入库', value: '入库' },
-                            { label: '合同运费', value: '合同运费' },
-                          ]} />
+                        <Form.Item {...field} name={[field.name, 'countType']}>
+                          <Select options={options} />
                         </Form.Item>
                       )
+                    }}
+                  </Form.Item>
+                </Col>
+                <Col flex='auto'>
+                  <Form.Item {...field} name={[field.name, 'comments']}>
+                    <Input />
+                  </Form.Item>
+                </Col>
+                <Col flex='20px'>
+                  <Button type='text' onClick={() => remove(field.key)} icon={<MinusCircleOutlined />} />
+                </Col>
+              </Row>
+            ))}
+              <Row>
+                <Col span={24}>
+                  <Button block type='dashed' onClick={() => add()} icon={<PlusCircleOutlined />}>新增</Button>
+                </Col>
+              </Row>
 
-                    }
-                  }}
-                </Form.Item>
-              </Col>
-              <Col flex='200px'>
-                <RefCascader
-                  item={{ required: true, name: [field.name, 'other'], option: { ref: 'other', label: 'name', value: 'id' } }}/>
-              </Col>
-              <Col flex='100px'>
-                <Form.Item {...field} name={[field.name, 'unitPrice']}>
-                  <Input />
-                </Form.Item>
-              </Col>
-              <Col flex='110px'>
-                <Form.Item {...field} name={[field.name, 'countType']}>
-                  <Select options={[
-                    { label: '数量', value: '数量' },
-                    { label: '换算数量', value: '换算数量' },
-                    { label: '重量', value: '重量' },
-                    { label: '实际重量', value: '实际重量' },
-                  ]} />
-                </Form.Item>
-              </Col>
-              <Col flex='auto'>
-                <Form.Item {...field} name={[field.name, 'comments']}>
-                  <Input />
-                </Form.Item>
-              </Col>
-              <Col flex='20px'>
-                <Button type='text' onClick={() => remove(field.key)} icon={<MinusCircleOutlined />} />
-              </Col>
-            </Row>
-          ))
-          }
-            <Row>
-              <Col span={24}>
-                <Button block type='dashed' onClick={() => add()} icon={<PlusCircleOutlined />}>新增</Button>
-              </Col>
-            </Row>
-
-          </>
-        )
-      }}
-    </Form.List>
-  </Card>
-)
+            </>
+          )
+        }}
+      </Form.List>
+    </Card>
+  )
+}
