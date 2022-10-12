647fdc2157c1d22922ecc5ddd010dddc6f055f44
sealday <sealday@gmail.com>
2022-10-13 01:57:27 +0800

    feat: mono repo

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,8 +4,10 @@
  *
  * @format
  */
+const path = require('path');
 
 module.exports = {
+  watchFolders: [path.resolve(__dirname, '../node_modules')],
   transformer: {
     getTransformOptions: async () => ({
       transform: {
