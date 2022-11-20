d4337b697b5b46d5229c953e8318ed13129116ed
bai.jingfeng <bai.jingfeng@foxmail.com>
2022-11-20 23:21:32 +0800

    chore: 精简化处理

--- web/src/utils/version.js
+++ web/src/utils/version.js
@@ -2,13 +2,5 @@
 import { formatDate } from './date'
 const packageConfig = require("../../package.json")
 
-const getVersionNumber = () => {
-    return packageConfig?.version
-}
-
-const getVersionTime = () => {
-    return formatDate("yyyy-MM-dd hh:mm:ss")
-}
-
-export const versionNumber = getVersionNumber()
-export const versionTime = getVersionTime()
+export const versionNumber = packageConfig?.version
+export const versionTime = formatDate("yyyy-MM-dd hh:mm:ss")
