57e66d0ce639dbd2850b8d6a6e9a7495fcb56863
sealday <sealday@gmail.com>
2022-12-03 02:51:18 +0800

    fix: 修复 import 出错，bump 版本

--- server/src/app/settings/settings.service.ts
+++ server/src/app/settings/settings.service.ts
@@ -1,12 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
-
-import { Setting } from '../app.service';
+import { Setting } from 'src/schemas/setting.schema';
 
 @Injectable()
 export class SettingsService {
-  constructor(@InjectModel('Setting') private settingModel: Model<Setting>) { }
+  constructor(@InjectModel(Setting.name) private settingModel: Model<Setting>) { }
 
   async getLatestSettings() {
     const settings = await this.settingModel.findOne({}).sort({_id: -1})
