8ccc5811cc68b22fa500f938c2f933c9210672a3
sealday <sealday@gmail.com>
2022-12-07 00:45:09 +0800

    feat: 打印预览

--- web/src/components/button/modal-print-preview.button.js
+++ web/src/components/button/modal-print-preview.button.js
@@ -6,6 +6,7 @@ import { Viewer, Worker } from '@react-pdf-viewer/core';
 import { defaultLayoutPlugin } from '@react-pdf-viewer/default-layout';
 import '@react-pdf-viewer/core/lib/styles/index.css';
 import '@react-pdf-viewer/default-layout/lib/styles/index.css';
+import zh_CN from '@react-pdf-viewer/locales/lib/zh_CN.json';
 import { getAuthToken } from "utils";
 
 export default ({ pdf, title, content, children, ...btnProps }) => {
@@ -33,7 +34,14 @@ export default ({ pdf, title, content, children, ...btnProps }) => {
           open={open}
           onClose={hideModal}
           extra={
-            <Button type='primary' icon={<PrinterOutlined />} onClick={() => printFrame.current.print()}>打印</Button>
+            <Button type='primary' icon={<PrinterOutlined />} onClick={() => {
+              if (pdf) {
+                defaultLayoutPluginInstance.toolbarPluginInstance.printPluginInstance.print()
+              } else {
+                printFrame.current.print();
+              }
+            }
+            }>打印</Button>
           }
         >
           {
@@ -41,9 +49,9 @@ export default ({ pdf, title, content, children, ...btnProps }) => {
               ?
               <Worker workerUrl="https://unpkg.com/pdfjs-dist@3.1.81/build/pdf.worker.min.js">
                 <Viewer
+                  localization={zh_CN}
                   fileUrl={pdf + '?token=' + getAuthToken()}
                   plugins={[
-                    // Register plugins
                     defaultLayoutPluginInstance,
                   ]}
                 />
