27d03f0a1a33cd61de2b539cede63dad6d706129
bai.jingfeng <bai.jingfeng@foxmail.com>
2023-04-29 09:42:53 +0800

    dev: xlsx, 初步梳理导出逻

--- web/src/utils/excel.js
+++ web/src/utils/excel.js
@@ -1,9 +1,158 @@
 import { dateFormat } from 'hera-core'
 import { saveAs } from 'file-saver'
-import XLSX_STYLE from 'xlsx-style'
+// import XLSX_STYLE from 'xlsx'
 // 使用前端库 SheetJS-xlsx导出excel文件
 // 文档地址：https://docs.sheetjs.com/docs/
 // 为了定制样式，参考 https://juejin.cn/post/6903820868859002888#heading-8 改造导出excel的方法
+// 导出excel表格
+export const exportExcel = (XLSX, data, name) => {
+  console.log(
+    '%c Line:158 🍬 XLSX',
+    'font-size:18px;color:#4fff4B;background:#6ec1c2',
+    XLSX
+  )
+
+  // 创建 workbook 对象
+  const workbook = XLSX.utils.book_new()
+
+  // 获取创建好的电子表格对象，已经设置好单元格样式，字体样式等细化配置
+  const worksheet = getWorksheetWithStyle(XLSX, data)
+
+  // 创建工作簿对象并添加电子表格
+  XLSX.utils.book_append_sheet(workbook, worksheet, '结算表')
+
+  // 导出 Excel
+  XLSX.writeFile(workbook, `${name}.xlsx`)
+  // 将工作簿对象导出为Excel文件
+  // downLoadExcel(workbook, name)
+}
+
+// 配置excel表格样式
+const getWorksheetWithStyle = (XLSX, rawData) => {
+  // 构建表头数据
+  const header = [['华东公司料具租赁站'], ['料具租赁费用结算单']]
+  // 创建 worksheet 对象
+  // 将表头插入到第一二行
+  const worksheet = XLSX.utils.aoa_to_sheet(header)
+  // 合并单元格
+  mergeCells(XLSX, worksheet)
+  XLSX.utils.sheet_add_aoa(worksheet, [[`结算时段:${'金额占位'}`]], {
+    origin: { r: 2, c: 0 },
+  })
+  XLSX.utils.sheet_add_aoa(worksheet, [[`工程名称:${'金额占位'}`]], {
+    origin: { r: 2, c: 6 },
+  })
+
+  // 获取表格主要内容数据
+  const data = getRegularAoaData(rawData)
+  XLSX.utils.sheet_add_aoa(worksheet, data, { origin: { r: 3, c: 0 } })
+  // 将表尾插入到最后一行
+  XLSX.utils.sheet_add_aoa(
+    worksheet,
+    [['华东公司料具租赁站'], ['料具租赁费用结算单']],
+    { origin: -1 }
+  )
+
+  // Object.keys(worksheet).forEach(key => {
+  //   worksheet[key].s = {
+  //     border: {
+  //       //边框
+  //       // bottom: { style: 'thin', color: 'FFd4d4d4' },
+  //       left: { style: 'thin', color: '00D4D4D4' },
+  //       top: { style: 'thin', color: '00D4D4D4' },
+  //       // right: { style: 'thin', color: 'FFD4D4D4' },
+  //     },
+  //     font: {
+  //       //字体
+  //       name: '宋体',
+  //       sz: 14,
+  //       bold: false,
+  //       italic: false,
+  //       color: { auto: 1 },
+  //     },
+  //     alignment: {
+  //       // 对齐方式，居中
+  //       horizontal: 'center',
+  //       vertical: 'center',
+  //       wrapText: true,
+  //     },
+  //     fill: {
+  //       //填充
+  //       fgColor: { rgb: 'FFFFFFFF' },
+  //     },
+  //   }
+  // })
+  console.log(
+    '%c Line:240 🥐 worksheet',
+    'font-size:18px;color:#2eafb0;background:#ffdd4d',
+    worksheet
+  )
+
+  return worksheet
+}
+
+// 合并单元格
+const mergeCells = (XLSX,worksheet) => {
+  if (!worksheet['!merges']) worksheet['!merges'] = []
+  worksheet['!merges'].push(
+    XLSX.utils.decode_range('A1:L1'),
+    XLSX.utils.decode_range('A2:L2'),
+    XLSX.utils.decode_range('A3:F3'),
+    XLSX.utils.decode_range('G3:L3')
+  )
+
+}
+
+// 获取表格内容数据，将原始数据转换为二维数组，js对象形式有问题
+const getRegularAoaData = rawData => {
+  // 表格属性名称
+  const headers = [
+    '料具名称',
+    '料具规格',
+    '类别',
+    '起租日期',
+    '结算日期',
+    '数量',
+    '单位',
+    '单价(元)',
+    '天数',
+    '金额(元)',
+    '同类累计金额',
+    '备注',
+  ]
+  const data = [
+    ...(rawData?.history || []).map(item => [
+      item.name, // 料具名称
+      null,
+      null,
+      dateFormat(rawData?.start), // 起租日期
+      dateFormat(rawData?.end), // 结算日期
+      item.count,
+      item.unit,
+      item.unitPrice,
+      item.days,
+      item.price,
+      null,
+      null,
+    ]),
+    ...(rawData?.list || []).map(item => [
+      item.name, // 料具名称
+      null,
+      null,
+      dateFormat(item.inOut === '入库' ? rawData?.start : item.outDate), // 起租日期
+      dateFormat(item.inOut === '入库' ? item.outDate : rawData?.end), // 结算日期
+      item.count,
+      item.unit,
+      item.unitPrice,
+      item.days,
+      item.price,
+      null,
+      null,
+    ]),
+  ]
+  return [headers, ...data]
+}
+
 export const rentExcelExportNewFunc = (XLSX, rent, name) => {
   const wb = XLSX.utils.book_new()
   // const jsonConfigArr = [
@@ -153,147 +302,7 @@ export const rentExcelExportNewFunc = (XLSX, rent, name) => {
   )
 }
 
-// 导出excel表格
-export const exportExcel = (XLSX, data, name) => {
-  console.log(
-    '%c Line:158 🍬 XLSX',
-    'font-size:18px;color:#4fff4B;background:#6ec1c2',
-    XLSX
-  )
-  // 获取创建好的电子表格对象，已经设置好单元格样式，字体样式等细化配置
-  const worksheet = getWorksheetWithStyle(XLSX, data)
-  // 创建工作簿对象并添加电子表格
-  const workbook = XLSX.utils.book_new()
-  XLSX.utils.book_append_sheet(workbook, worksheet, '结算表')
 
-  // 将工作簿对象导出为Excel文件
-  downLoadExcel(workbook, name)
-}
-
-// 配置excel表格样式
-const getWorksheetWithStyle = (XLSX, rawData) => {
-  // 构建表头数据
-  const header = [['华东公司料具租赁站'], ['料具租赁费用结算单']]
-  // 将表头插入到第一二行
-  const worksheet = XLSX.utils.aoa_to_sheet(header)
-  // 合并单元格
-  if (!worksheet['!merges']) worksheet['!merges'] = []
-  worksheet['!merges'].push(
-    XLSX.utils.decode_range('A1:L1'),
-    XLSX.utils.decode_range('A2:L2'),
-    XLSX.utils.decode_range('A3:F3'),
-    XLSX.utils.decode_range('G3:L3')
-  )
-  XLSX.utils.sheet_add_aoa(worksheet, [[`结算时段:${'金额占位'}`]], {
-    origin: { r: 2, c: 0 },
-  })
-  XLSX.utils.sheet_add_aoa(worksheet, [[`工程名称:${'金额占位'}`]], {
-    origin: { r: 2, c: 6 },
-  })
-
-  console.log(
-    '%c Line:199 🥑 worksheet',
-    'font-size:18px;color:#465975;background:#fca650',
-    worksheet
-  )
-
-  // 获取表格主要内容数据
-  const data = getRegularAoaData(rawData)
-  XLSX.utils.sheet_add_aoa(worksheet, data, { origin: { r: 3, c: 0 } })
-  // 将表尾插入到最后一行
-  XLSX.utils.sheet_add_aoa(
-    worksheet,
-    [['华东公司料具租赁站'], ['料具租赁费用结算单']],
-    { origin: -1 }
-  )
-
-  // Object.keys(worksheet).forEach(key => {
-  //   worksheet[key].s = {
-  //     border: {
-  //       //边框
-  //       // bottom: { style: 'thin', color: 'FFd4d4d4' },
-  //       left: { style: 'thin', color: '00D4D4D4' },
-  //       top: { style: 'thin', color: '00D4D4D4' },
-  //       // right: { style: 'thin', color: 'FFD4D4D4' },
-  //     },
-  //     font: {
-  //       //字体
-  //       name: '宋体',
-  //       sz: 14,
-  //       bold: false,
-  //       italic: false,
-  //       color: { auto: 1 },
-  //     },
-  //     alignment: {
-  //       // 对齐方式，居中
-  //       horizontal: 'center',
-  //       vertical: 'center',
-  //       wrapText: true,
-  //     },
-  //     fill: {
-  //       //填充
-  //       fgColor: { rgb: 'FFFFFFFF' },
-  //     },
-  //   }
-  // })
-  console.log(
-    '%c Line:240 🥐 worksheet',
-    'font-size:18px;color:#2eafb0;background:#ffdd4d',
-    worksheet
-  )
-
-  return worksheet
-}
-
-// 获取表格内容数据，将原始数据转换为二维数组，js对象形式有问题
-const getRegularAoaData = rawData => {
-  // 表格属性名称
-  const headers = [
-    '料具名称',
-    '料具规格',
-    '类别',
-    '起租日期',
-    '结算日期',
-    '数量',
-    '单位',
-    '单价(元)',
-    '天数',
-    '金额(元)',
-    '同类累计金额',
-    '备注',
-  ]
-  const data = [
-    ...(rawData?.history || []).map(item => [
-      item.name, // 料具名称
-      null,
-      null,
-      dateFormat(rawData?.start), // 起租日期
-      dateFormat(rawData?.end), // 结算日期
-      item.count,
-      item.unit,
-      item.unitPrice,
-      item.days,
-      item.price,
-      null,
-      null,
-    ]),
-    ...(rawData?.list || []).map(item => [
-      item.name, // 料具名称
-      null,
-      null,
-      dateFormat(item.inOut === '入库' ? rawData?.start : item.outDate), // 起租日期
-      dateFormat(item.inOut === '入库' ? item.outDate : rawData?.end), // 结算日期
-      item.count,
-      item.unit,
-      item.unitPrice,
-      item.days,
-      item.price,
-      null,
-      null,
-    ]),
-  ]
-  return [headers, ...data]
-}
 
 export const exportExcelFunc = (XLSX, data, name) => {
   // 获取工作表对象，已经设置好单元格样式，字体样式等细化配置
@@ -356,25 +365,25 @@ const sheetToWorkBook = (XLSX, { worksheet, sheetName }, isMultiple) => {
 }
 
 // FQ: 根据 workbook 导出 excel
-const downLoadExcel = (wb, name) => {
-  const out = XLSX_STYLE.write(wb, {
-    bookType: 'xlsx',
-    bookSST: false,
-    type: 'binary',
-    compression: true,
-  })
-  const blobData = s2ab(out)
-  saveAs(
-    new Blob([blobData], { type: 'application/octet-stream' }),
-    name + '.xlsx'
-  )
+// const downLoadExcel = (wb, name) => {
+//   const out = XLSX_STYLE.write(wb, {
+//     bookType: 'xlsx',
+//     bookSST: false,
+//     type: 'binary',
+//     compression: true,
+//   })
+//   const blobData = s2ab(out)
+//   saveAs(
+//     new Blob([blobData], { type: 'application/octet-stream' }),
+//     name + '.xlsx'
+//   )
 
-  // 创建工作簿对象并添加电子表格
-  // const workbook = XLSX_STYLE.utils.book_new()
-  // XLSX_STYLE.utils.book_append_sheet(workbook, worksheet, '结算表')
-  // 将工作簿对象导出为Excel文件
-  // XLSX_STYLE.writeFile(wb, `${name}.xlsx`)
-}
+//   // 创建工作簿对象并添加电子表格
+//   // const workbook = XLSX_STYLE.utils.book_new()
+//   // XLSX_STYLE.utils.book_append_sheet(workbook, worksheet, '结算表')
+//   // 将工作簿对象导出为Excel文件
+//   // XLSX_STYLE.writeFile(wb, `${name}.xlsx`)
+// }
 
 // FQ: 构建数据
 const s2ab = s => {
