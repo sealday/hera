8c743fc99758068ad4e942058aad77b4ffdb9b36
sealday <sealday@gmail.com>
2023-06-13 11:40:20 +0800

    fix: 对外公司名称可选择

--- web/src/pages/system/ProjectForm.js
+++ web/src/pages/system/ProjectForm.js
@@ -3,8 +3,18 @@ import React from 'react'
 import { Card, Form } from 'antd'
 import { genFormContent } from '../../utils/antd'
 import projectSchema from '../../schema/project.schema'
+import { useSelector } from 'react-redux'
 
 const ProjectForm = ({ form, initialValues, onSubmit }) => {
+  const config = useSelector(state => state.system.config)
+  projectSchema.forEach(item => {
+    if (item.name === 'associatedCompany') {
+      item.option = {
+        type: 'static_value_only',
+        values: config.externalNames,
+      }
+    }
+  })
   const formContent = genFormContent(projectSchema, 2)
 
   return (
