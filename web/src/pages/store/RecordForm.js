e72edeb9caa2fac43fa320846f1d61bc70a38b59
wjh <daoyoucloud@daoyouclouddeMac-mini.local>
2023-10-16 17:37:44 +0800

    仓库出入库重量必填

--- web/src/pages/store/RecordForm.js
+++ web/src/pages/store/RecordForm.js
@@ -1,14 +1,25 @@
-import { AutoComplete, Card, Checkbox, Col, DatePicker, Form, Input, Row, Select, Switch } from "antd"
-import _ from "lodash"
-import moment from "moment"
-import { RefSelect } from "../../components"
-import { RECORD_CLIENT_TYPES } from "../../utils"
-import React, { useContext } from 'react';
-import EntryForm from "./records/entry.form"
-import ComplementForm from "./records/complement.form"
-import { SettingContext } from "./records"
-import { CAR_NUMBERS } from "../../constants"
-import AdditionalForm from "./records/additional.form"
+import {
+  AutoComplete,
+  Card,
+  Checkbox,
+  Col,
+  DatePicker,
+  Form,
+  Input,
+  Row,
+  Select,
+  Switch,
+} from 'antd'
+import _, { values } from 'lodash'
+import moment from 'moment'
+import { RefSelect } from '../../components'
+import { RECORD_CLIENT_TYPES } from '../../utils'
+import React, { useContext } from 'react'
+import EntryForm from './records/entry.form'
+import ComplementForm from './records/complement.form'
+import { SettingContext } from './records'
+import { CAR_NUMBERS } from '../../constants'
+import AdditionalForm from './records/additional.form'
 
 const styles = {
   block: { width: '100%' },
@@ -20,83 +31,151 @@ export default ({ form, initialValues, onSubmit }) => {
   const settings = useContext(SettingContext)
   const type = Form.useWatch('type', form)
   const projectItem = {
-    label: '仓库', name: 'projectId', type: 'text', required: true,
+    label: '仓库',
+    name: 'projectId',
+    type: 'text',
+    required: true,
     option: {
-      type: 'ref', ref: 'project', label: 'name', value: '_id',
+      type: 'ref',
+      ref: 'project',
+      label: 'name',
+      value: '_id',
     },
     filter: {
-      key: 'type', value: type,
+      key: 'type',
+      value: type,
     },
   }
   return (
-    <Form onFinish={onSubmit} form={form} colon={false} labelCol={{ flex: '5em' }} labelWrap initialValues={initialValues}>
-      <Card bordered={false} title='基础信息'>
+    <Form
+      onFinish={onSubmit}
+      form={form}
+      colon={false}
+      labelCol={{ flex: '5em' }}
+      labelWrap
+      initialValues={initialValues}
+    >
+      <Card bordered={false} title="基础信息">
         <Row gutter={24}>
-          {settings.project ? <Col span={8}>
-            <Form.Item label="类别" name='type' rules={rules}>
-              <Select>
-                {RECORD_CLIENT_TYPES.map(name => <Select.Option key={name}>{name}</Select.Option>)}
-              </Select>
-            </Form.Item>
-          </Col> : null}
-          {settings.project ? <Col span={8}>
-            <RefSelect item={projectItem} />
-          </Col> : null}
+          {settings.project ? (
+            <Col span={8}>
+              <Form.Item label="类别" name="type" rules={rules}>
+                <Select>
+                  {RECORD_CLIENT_TYPES.map(name => (
+                    <Select.Option key={name}>{name}</Select.Option>
+                  ))}
+                </Select>
+              </Form.Item>
+            </Col>
+          ) : null}
+          {settings.project ? (
+            <Col span={8}>
+              <RefSelect item={projectItem} />
+            </Col>
+          ) : null}
           <Col span={8}>
-            <Form.Item label="日期" name='outDate' rules={rules}>
+            <Form.Item label="日期" name="outDate" rules={rules}>
               <DatePicker style={styles.block} />
             </Form.Item>
           </Col>
-          {settings.originalOrder ? <Col span={8}>
-            <Form.Item label="原始单号" name='originalOrder'>
-              <Input />
-            </Form.Item>
-          </Col> : null}
-          {settings.carNumber ? <Col span={8}>
-            <Form.Item label="车号" name='carNumber'>
-              <AutoComplete options={CAR_NUMBERS.map(item => ({ label: item, value: item }))} />
-            </Form.Item>
-          </Col> : null}
-          {settings.weight ? <Col span={8}>
-            <Form.Item label="重量" name='weight'>
-              <Input suffix='吨' />
-            </Form.Item>
-          </Col> : null}
-          {settings.freight ? <Col span={8}>
-            <Form.Item label="合同运费" name='freight' valuePropName="checked">
-              <Switch />
-            </Form.Item>
-          </Col> : null}
+          {settings.originalOrder ? (
+            <Col span={8}>
+              <Form.Item label="原始单号" name="originalOrder">
+                <Input />
+              </Form.Item>
+            </Col>
+          ) : null}
+          {settings.carNumber ? (
+            <Col span={8}>
+              <Form.Item label="车号" name="carNumber">
+                <AutoComplete
+                  options={CAR_NUMBERS.map(item => ({
+                    label: item,
+                    value: item,
+                  }))}
+                />
+              </Form.Item>
+            </Col>
+          ) : null}
+          {settings.weight ? (
+            <Col span={8}>
+              <Form.Item
+                label="重量"
+                name="weight"
+                rules={[
+                  { required: true },
+                  {
+                    validator: (rule, value, callback) => {
+                      if (value && isNaN(value)) {
+                        callback('重量必须是数字')
+                      } else if (value && value.length > 5) {
+                        callback('数字不能超过三位小数')
+                      } else {
+                        callback()
+                      }
+                    },
+                  },
+                ]}
+              >
+                <Input suffix="吨" />
+              </Form.Item>
+            </Col>
+          ) : null}
+          {settings.freight ? (
+            <Col span={8}>
+              <Form.Item
+                label="合同运费"
+                name="freight"
+                valuePropName="checked"
+              >
+                <Switch />
+              </Form.Item>
+            </Col>
+          ) : null}
           <Col span={24}>
-            <Form.Item label="备注" name='comments'>
+            <Form.Item label="备注" name="comments">
               <Input.TextArea rows={2} />
             </Form.Item>
           </Col>
         </Row>
       </Card>
-      <Card bordered={false} title='明细信息' style={styles.keepSpace}>
+      <Card bordered={false} title="明细信息" style={styles.keepSpace}>
         <Row>
           <Col span={24}>
             <Form.List name="entries">
-              {(fields, operation, meta) => <EntryForm fields={fields} operation={operation} meta={meta} />}
+              {(fields, operation, meta) => (
+                <EntryForm fields={fields} operation={operation} meta={meta} />
+              )}
             </Form.List>
           </Col>
         </Row>
       </Card>
-      <Card bordered={false} title='赔偿维修信息' style={styles.keepSpace}>
+      <Card bordered={false} title="赔偿维修信息" style={styles.keepSpace}>
         <Row>
           <Col span={24}>
-            <Form.List name="complements" >
-              {(fields, operation, meta) => <ComplementForm fields={fields} operation={operation} meta={meta} />}
+            <Form.List name="complements">
+              {(fields, operation, meta) => (
+                <ComplementForm
+                  fields={fields}
+                  operation={operation}
+                  meta={meta}
+                />
+              )}
             </Form.List>
           </Col>
         </Row>
       </Card>
-      <Card bordered={false} title='额外信息' style={styles.keepSpace}>
+      <Card bordered={false} title="额外信息" style={styles.keepSpace}>
         <Row>
           <Col span={24}>
-            <Form.List name="additionals" >
-              {(fields, operation, meta) => <AdditionalForm fields={fields} operation={operation} meta={meta} />}
+            <Form.List name="additionals">
+              {(fields, operation, meta) => (
+                <AdditionalForm
+                  fields={fields}
+                  operation={operation}
+                  meta={meta}
+                />
+              )}
             </Form.List>
           </Col>
         </Row>
